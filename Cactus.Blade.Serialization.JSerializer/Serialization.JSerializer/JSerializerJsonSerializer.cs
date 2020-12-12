b954463e982bf9fe19ea7631ff72af93976c917c
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-12 18:51:02 +0330

    Create JSerializerJsonSerializer.cs

---
+++ Cactus.Blade.Serialization.JSerializer/Serialization.JSerializer/JSerializerJsonSerializer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace Serialization.JSerializer
+{
+    /// <summary>
+    /// A JSON implementation of the <see cref="ISerializer"/> interface using <see cref="DataContractJsonSerializer"/>/>.
+    /// </summary>
+    public class JSerializerJsonSerializer : ISerializer
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataContractJsonSerializer"/> class.
+        /// </summary>
+        /// <param name="name">The name of the serializer, used to when selecting which serializer to use.</param>
+        /// <param name="settings">Options for customizing the JsonSerializer.</param>
+        public JSerializerJsonSerializer(string name = "default", DataContractJsonSerializerSettings settings = null)
+        {
+            Name = name ?? "default";
+            Settings = settings;
+        }
+
+        /// <inheritdoc />
+        public string Name { get; }
+
+        /// <summary>
+        /// Gets the <see cref="DataContractJsonSerializerSettings"/> options.
+        /// </summary>
+        public DataContractJsonSerializerSettings Settings { get; }
+
+        /// <inheritdoc />
+        public void SerializeToStream(Stream stream, object item, Type type)
+        {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+
+            var serializer = Settings == null
+                ? new SystemJson.DataContractJsonSerializer(type)
+                : new SystemJson.DataContractJsonSerializer(type, Settings);
+
+            serializer.WriteObject(stream, item);
+        }
+
+        /// <inheritdoc />
+        public object DeserializeFromStream(Stream stream, Type type)
+        {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+
+            var serializer = Settings == null
+                ? new SystemJson.DataContractJsonSerializer(type)
+                : new SystemJson.DataContractJsonSerializer(type, Settings);
+
+            return serializer.ReadObject(stream);
+        }
+
+        /// <inheritdoc />
+        public string SerializeToString(object item, Type type)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+
+            var serializer = Settings == null
+                ? new SystemJson.DataContractJsonSerializer(type)
+                : new SystemJson.DataContractJsonSerializer(type, Settings);
+
+            using (var stream = new MemoryStream())
+            {
+                serializer.WriteObject(stream, item);
+                stream.Flush();
+                return Encoding.UTF8.GetString(stream.ToArray());
+            }
+        }
+
+        /// <inheritdoc />
+        public object DeserializeFromString(string data, Type type)
+        {
+            if (data == null)
+                throw new ArgumentNullException(nameof(data));
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+
+            var serializer = Settings == null
+                ? new SystemJson.DataContractJsonSerializer(type)
+                : new SystemJson.DataContractJsonSerializer(type, Settings);
+
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(data)))
+            {
+                return serializer.ReadObject(stream);
+            }
+        }
+    }
+}
