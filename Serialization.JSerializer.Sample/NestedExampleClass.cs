fc584092eb6a64496e7d0c921c3cd9c4bd84a4cc
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-13 10:26:42 +0330

    Add Serialization.JSerializer.Sample

---
+++ Serialization.JSerializer.Sample/NestedExampleClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace Serialization.JSerializer.Sample
+{
+    [DataContract]
+    public class NestedExampleClass
+    {
+        [DataMember]
+        public int IntProperty { get; set; }
+        [DataMember]
+        public bool BoolProperty { get; set; }
+        [DataMember]
+        public string StringProperty { get; set; }
+    }
+}
---
+++ Serialization.JSerializer.Sample/ParentExampleClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Serialization.JSerializer.Sample
+{
+    [DataContract]
+    public class ParentExampleClass
+    {
+        [DataMember]
+        public DateTime DateProperty { get; set; }
+        [DataMember]
+        public NestedExampleClass ClassProperty { get; set; }
+    }
+}
